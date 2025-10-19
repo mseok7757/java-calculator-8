4d87adc8f39cc27ff106ca059ab7bd75291c50f2
Myeongseok Kim <myeongseokkim@Myeongseokui-MacBookPro.local>
2025-10-19 17:35:56 +0900

    커스텀 구분자 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,6 +1,8 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
 //import java.io.Console;
 
 public class Application {
@@ -16,14 +18,27 @@ public class Application {
     //문자열 기본 구분자로 계산
     public static int calculate(String input){
 
-        // 쉼표와 클론을 구분자로 처리후 numbers에 담기
-        String[] numbers = input.split("[,:]");
+        String[] numbers;
+
+        //입력값의 첫번째 값이 //이고 \n의 인덱스가 3일경우 실행
+        if (input.startsWith("//") && input.indexOf("\\n")==3){
+
+            //커스텀 구분자를 추출
+            char customSeparator = input.charAt(2);
+            //숫자부분부터만 담기
+            input = input.substring(input.indexOf("n") + 1);
+            numbers= input.split("[,:" + customSeparator + "]");
+        } else {
+            numbers = input.split("[,:]");
+        }
 
         // 정수형으로 변환후 합산
         int sum = 0;
         for (String number : numbers){
             sum += Integer.parseInt(number);
         }
+
+
 
         return sum;
 
